53f27d7ecd07583530e02fadd6fd85e0cb9c48df
Ainetos <airupim@gmail.com>
2026-01-26 01:40:15 +0100

    colores

--- script.js
+++ script.js
@@ -4,4 +4,26 @@ function Healthbar (vida, vidaMax) {
     document.getElementById("barra").style.width=vidaRes+"%";
 
     document.getElementById("TextoVida").innerHTML= vida+" /"+vidaMax;
+
+    if(vidaRes>50){
+        document.getElementById("barra").style.backgroundColor="limegreen";
+        document.getElementById("inferior").style.backgroundColor="darkgreen";
+    }
+    else if(vidaRes>25){
+        document.getElementById("barra").style.backgroundColor="orange";
+        document.getElementById("inferior").style.backgroundColor="Chocolate";
+    }
+    else if(vidaRes>0){
+        document.getElementById("barra").style.backgroundColor="red";
+        document.getElementById("inferior").style.backgroundColor="darkred";
+    }
+    else{
+        document.getElementById("barra").style.backgroundColor="gray";
+        document.getElementById("inferior").style.backgroundColor="black";
+    }
+}
+
+function CambiarColores(colorTexto, colorBordeTexto){
+    document.getElementById("TextoVida").style.color=colorTexto;
+    document.getElementById("TextoVida").style= 'text-shadow: -2px -2px 0 ' + colorBordeTexto + ', 2px -2px 0 ' + colorBordeTexto + ', -2px 2px 0 ' + colorBordeTexto + ', 2px 2px 0 ' + colorBordeTexto + ', -3px 0px 0 ' + colorBordeTexto + ', 3px 0px 0 ' + colorBordeTexto + ', 0px -3px 0 ' + colorBordeTexto + ', 0px 3px 0 ' + colorBordeTexto +';';
 }
