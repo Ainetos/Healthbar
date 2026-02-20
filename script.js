cc4ab720d4aec0f990ac536b4d13be13bb829f11
Ainetos <airupim@gmail.com>
2026-02-20 23:45:09 +0100

    anima añadida

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ function Healthbar(vida, vidaMax) {
     document.getElementById("TextoVida").innerHTML = vida + " /" + vidaMax;
 }
 
+var animaRes
+function AnimaBar(anima, animaMax) {
+    animaRes = (anima * 100) / animaMax;
+
+    document.getElementById("barraAnima").style.width = animaRes + "%";
+
+    document.getElementById("TextoAnima").innerHTML = anima + " /" + animaMax;
+}
+
 function CambiarColores(colorTexto, colorBordeTexto) {
     document.getElementById("TextoVida").style = 'color: ' + colorTexto + ';margin-top: -17px; margin-left: 24px; font-size: 92px; text-shadow: -2px -2px 0 ' + colorBordeTexto + ', 2px -2px 0 ' + colorBordeTexto + ', -2px 2px 0 ' + colorBordeTexto + ', 2px 2px 0 ' + colorBordeTexto + ', -3px 0px 0 ' + colorBordeTexto + ', 3px 0px 0 ' + colorBordeTexto + ', 0px -3px 0 ' + colorBordeTexto + ', 0px 3px 0 ' + colorBordeTexto + ';';
 }
